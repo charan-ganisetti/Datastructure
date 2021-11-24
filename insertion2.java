3ddbef245556b9ab61f2078d056dcb9b925c92b6
charan542 <84331762+charan542@users.noreply.github.com>
2021-11-24 07:38:32 +0530

    Add files via upload

---
+++ insertion2.java
@@ -0,0 +1,39 @@
+public class insertion2{
+    public static void main(String[] args){
+        //int a[]={10,34,6,2,45,23,56,32};
+        int a[]={1,2,3,4};
+        int i,j,k,temp,min;
+        System.out.print("array elements before soring ");
+        for(k=0;k<a.length;k++){
+        System.out.print(a[k]+" ");
+       
+        }
+        System.out.println();
+        for(i=1;i<a.length;i++){
+              temp=a[i];
+              
+            for(j=i-1;j>=0;j--){
+            
+                if(temp<a[j]){
+                    if(j==0){
+                        a[j+1]=a[j];
+                        a[j]=temp;
+                    }
+                    else{
+                        a[j+1]=a[j];
+                    }
+                }
+                else{
+                     a[j+1]=temp;
+                     break;
+                }
+            
+            }
+        }
+    System.out.print("array elements after sorting ");
+    for(k=0;k<a.length;k++){
+        System.out.print(a[k]+" ");
+    }
+    
+    }
+}
